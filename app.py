f828fa7f284fd8bbd353f666f9b495faca2a8e52
AlessandroHI <85206440+AlessandroHI@users.noreply.github.com>
2022-04-05 16:05:34 -0600

    Clase Main
    Actualizacion de datos

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from pickle import GLOBAL
 from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
@@ -6,19 +7,22 @@ from xml.etree import ElementTree
 from Lista import Lista
 from Nodo import Nodo
 from Ciudad import Ciudad
-from Casilla import Casilla #VALOR DE LA MATRIZ Y CARACTERISTICA
-from Recurso import Recurso 
+from Casilla import Casilla  # VALOR DE LA MATRIZ Y CARACTERISTICA
+from Recurso import Recurso
 from UnidadCivil import UnidadCivil
 from Entrada import Entrada
 from UnidadesMilitar import UnidadesMilitar
 from Robot import Robot
 from PIL import ImageTk, Image
 
+
 def cofiguracion(ruta): #ANALIZADOR DEL ARCHIVO 
   global Lista_ciudades, Lista_robots #SE HACE EL LLAMADO DE LAS VARIABLES GLOBALES GENERALES
 
   archivoXML = ElementTree.parse(ruta)
   root = archivoXML.getroot()
+  for item in table1.get_children():
+        table1.delete(item)
 
 #----------------------- SE AGREGAN CIUDAD A LISTA DE CIUDADES ----------------
   for ciudades in root.iter("listaCiudades"):
@@ -29,6 +33,15 @@ def cofiguracion(ruta): #ANALIZADOR DEL ARCHIVO
         name = nombre.text
         Nofilas = nombre.attrib["filas"]
         Nocolumnas = nombre.attrib["columnas"]
+
+        if Lista_ciudades.length() > 0:
+          for i in range(Lista_ciudades.length()):
+            ciudadName = Lista_ciudades[i].nombre
+            if name == ciudadName:
+              Lista_ciudades.reset(i)
+              break
+        
+
 
         listaRecursos = Lista()
         listaCiviles = Lista()
@@ -111,15 +124,24 @@ def cofiguracion(ruta): #ANALIZADOR DEL ARCHIVO
     for robot in robots.iter("nombre"):
       nombre = robot.text
       tipo = robot.attrib["tipo"]
+      
+      if Lista_robots.length() > 0:
+          for i in range(Lista_robots.length()):
+            robotdName = Lista_robots[i].nombre
+            if nombre == robotdName:
+              Lista_robots.reset(i)
+              break
+        
+
       if tipo == "ChapinFighter":
         capacidad = robot.attrib["capacidad"]
       else:
         capacidad = "0"
       Lista_robots.append(Robot(nombre,tipo,int(capacidad)))
-  
-          
-def Buscar_archivo():  # FUNCION QUE REALIZAR LA SELECCION DEL ARCHIVO A CARGAR 
-  
+
+
+def Buscar_archivo():  # FUNCION QUE REALIZAR LA SELECCION DEL ARCHIVO A CARGAR
+
     Tk().withdraw()
     archivo = filedialog.askopenfile(
         title="Seleccionar un archivo",
@@ -134,12 +156,14 @@ def Buscar_archivo():  # FUNCION QUE REALIZAR LA SELECCION DEL ARCHIVO A CARGAR
     else:
         return archivo
 
-def cargarPisos():#METODO PARA SELCCION DEL ARCHIVO
+
+def cargarPisos():  # METODO PARA SELCCION DEL ARCHIVO
     archivo = Buscar_archivo()
     cofiguracion(archivo)
 
+
 def graficarCiudad(ciudad):
-  inicio ="""digraph structs {
+    inicio = """digraph structs {
     node [shape=none fontname=Helvetica];
 
      struct1 [label=<<TABLE border="2" CELLBORDER="1" CELLSPACING="2" bgcolor="white" color="black">
@@ -147,122 +171,259 @@ def graficarCiudad(ciudad):
         <TD  BGCOLOR="white">     </TD>
         """
 
-#------------------------- COLUMNAS IDENTIFIACION --------------------------------
-  for i in range(ciudad.columna):
-    inicio +="""<TD  BGCOLOR="white">  """+str(i+1)+""" </TD>
+# ------------------------- COLUMNAS IDENTIFIACION --------------------------------
+    for i in range(ciudad.columna):
+        inicio += """<TD  BGCOLOR="white">  """+str(i+1)+""" </TD>
     """
-  inicio +="""</TR>
+    inicio += """</TR>
   """
-#---------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------
 
-#--------------------------- INTEGRANDO FILAS Y COLUMNAS*------------------------
-  for i in range(ciudad.fila):
-    inicio +="""<TR>
+# --------------------------- INTEGRANDO FILAS Y COLUMNAS*------------------------
+    for i in range(ciudad.fila):
+        inicio += """<TR>
         <TD  BGCOLOR="white">"""+str(i+1)+"""</TD>
     """
-    for j in range(ciudad.columna):
-      casilla = ciudad.obtener_elem(i,j).caracteristica
-      if casilla == "Intransitable":
-        inicio +="""<TD  BGCOLOR="black">   </TD>
+        for j in range(ciudad.columna):
+            casilla = ciudad.obtener_elem(i, j).caracteristica
+            if casilla == "Intransitable":
+                inicio += """<TD  BGCOLOR="black">   </TD>
         """
 
-      if casilla == "Transitable":
-        inicio +="""<TD  BGCOLOR="white">   </TD>
+            if casilla == "Transitable":
+                inicio += """<TD  BGCOLOR="white">   </TD>
         """
 
-      if casilla == "Entrada":
-        inicio +="""<TD  BGCOLOR="green">   </TD>
+            if casilla == "Entrada":
+                inicio += """<TD  BGCOLOR="green">   </TD>
         """
 
-      if casilla == "Civil":
-        inicio +="""<TD  BGCOLOR="#81FFE5">   </TD>
+            if casilla == "Civil":
+                inicio += """<TD  BGCOLOR="#81FFE5">   </TD>
         """
 
-      if casilla == "Recurso":
-        inicio +="""<TD  BGCOLOR="grey57">   </TD>
-        """
-        
-      if casilla == "Unidad Militar":
-        inicio +="""<TD  BGCOLOR="#F1724">   </TD>
+            if casilla == "Recurso":
+                inicio += """<TD  BGCOLOR="grey57">   </TD>
         """
 
-    inicio +="""</TR>"""
-  inicio +="""</TABLE>>];
+            if casilla == "Unidad Militar":
+                inicio += """<TD  BGCOLOR="#F1724">   </TD>
+        """
+
+        inicio += """</TR>"""
+    inicio += """</TABLE>>];
        }
    """
-#---------------------------------------------------------------------------------
-  
-  crearArchivo("CiudadGraphviz.dot",inicio)
-  os.system('dot.exe -Tpng CiudadGraphviz.dot -o Ciudad.png')
+# ---------------------------------------------------------------------------------
+
+    crearArchivo("CiudadGraphviz.dot", inicio)
+    os.system('dot.exe -Tpng CiudadGraphviz.dot -o Ciudad.png')
+
 
 def crearArchivo(ruta, contenido):
     archivo = open(ruta, 'w')
     archivo.write(contenido)
-    archivo.close  
-    
+    archivo.close
+
+
 def Ciudades():
     for i in range(Lista_ciudades.length()):
-        dimension =str(Lista_ciudades[i].fila)
-        dimension +="X"
-        dimension +=str(Lista_ciudades[i].columna)
-        table1.insert("", END,text=str(Lista_ciudades[i].nombre),values=(dimension))
+        dimension = str(Lista_ciudades[i].fila)
+        dimension += "X"
+        dimension += str(Lista_ciudades[i].columna)
+        table1.insert("", END, text=str(
+            Lista_ciudades[i].nombre), values=(dimension))
 
-def get_data(): # METODO PARA GRAFICAR CIUDADADES EN GUI
-  ciudad = table1.item(table1.selection())["text"]
-  for i in range(Lista_ciudades.length()):
-    nombre = Lista_ciudades[i].nombre
-    if nombre == ciudad:
-      graficarCiudad(Lista_ciudades[i])
-      MostrarImagen("Ciudad.png")
-  
-def MostrarImagen(ruta):
-  img = Image.open(ruta)
-  new_img = img.resize((420,380))
-  render = ImageTk.PhotoImage(new_img)
-  img2 = Label(ventana, image=render)
-  img2.image = render
-  img2.place(x=290, y = 90)
+
+def Robots():
+    for item in table2.get_children():
+        table2.delete(item)
+    tipoMison = comboMision.get()
+    for i in range(Lista_robots.length()):
+        tipo = str(Lista_robots[i].tipo)
+        capacidad = str(Lista_robots[i].capacidad)
+        if tipoMison == "Rescate" and tipo == "ChapinRescue":
+            table2.insert("", END, text=str(
+                Lista_robots[i].nombre), values=(tipo, capacidad))
+
+        if tipoMison == "Extraccion Recurso" and tipo == "ChapinFighter":
+            table2.insert("", END, text=str(
+                Lista_robots[i].nombre), values=(tipo, capacidad))
+
+
+def get_data():  # METODO PARA GRAFICAR CIUDADADES EN GUI
+    ciudad = table1.item(table1.selection())["text"]
+    entradas = []
+    for i in range(Lista_ciudades.length()):
+        nombre = Lista_ciudades[i].nombre
+        if nombre == ciudad:
+            graficarCiudad(Lista_ciudades[i])
+            MostrarImagen("Ciudad.png")
+            for j in range(Lista_ciudades[i].entradas.length()):
+                cordenada = "Fila: " + str(Lista_ciudades[i].entradas[j].y)
+                cordenada += " Columna: " + \
+                    str(Lista_ciudades[i].entradas[j].x)
+                entradas.append(cordenada)
+            comboEntradas["values"] = entradas
+
+
+def change_civiles():
+    mision = comboMision.get()
+    ciudad = table1.item(table1.selection())["text"]
+    unidades = []
+    for i in range(Lista_ciudades.length()):
+        nombre = Lista_ciudades[i].nombre
+        if nombre == ciudad and mision == "Rescate":
+            for j in range(Lista_ciudades[i].unidadCiviles.length()):
+                cordenada = "Fila: " + \
+                    str(Lista_ciudades[i].unidadCiviles[j].y)
+                cordenada += " Columna: " + \
+                    str(Lista_ciudades[i].unidadCiviles[j].x)
+                unidades.append(cordenada)
+            comboCiviles["values"] = unidades
+        if mision == "Extraccion Recurso":
+            comboCiviles["values"] = ["---"]
+
+
+def change_recurso():
+    mision = comboMision.get()
+    ciudad = table1.item(table1.selection())["text"]
+    recurso = []
+    for i in range(Lista_ciudades.length()):
+        nombre = Lista_ciudades[i].nombre
+        if nombre == ciudad and mision == "Extraccion Recurso":
+            for j in range(Lista_ciudades[i].recursos.length()):
+                cordenada = "Fila: " + str(Lista_ciudades[i].recursos[j].y)
+                cordenada += " Columna: " + \
+                    str(Lista_ciudades[i].recursos[j].x)
+                recurso.append(cordenada)
+            comboRecursos["values"] = recurso
+
+
+def MostrarImagen(ruta):  # METODO PARA GRAFICAR CIUDADADES EN GUI
+    img = Image.open(ruta)
+    new_img = img.resize((420, 380))
+    render = ImageTk.PhotoImage(new_img)
+    img2 = Label(ventana, image=render)
+    img2.image = render
+    img2.place(x=290, y=90)
 
 
 if __name__ == '__main__':
- global Lista_ciudades, Lista_robots
- Lista_ciudades = Lista()
- Lista_robots = Lista()
+    global Lista_ciudades, Lista_robots
+    Lista_ciudades = Lista()
+    Lista_robots = Lista()
 
- ventana = Tk()
- ventana.title("Chapin Warrios, S.A")
- btnCargar = Button(ventana, text="CARGAR CONFIGURACION", width=22,height=2, command=cargarPisos)
- btnCargar.config(fg="Black",bg="#FC7643",font=("Bahnschrift SemiBold",10))
- btnCargar.place(x= 10, y=10)
+    ventana = Tk()
+    ventana.title("Chapin Warrios, S.A")
+    btnCargar = Button(ventana, text="CARGAR CONFIGURACION",
+                       width=22, height=2, command=cargarPisos)
+    btnCargar.config(fg="Black", bg="#FC7643",
+                     font=("Bahnschrift SemiBold", 10))
+    btnCargar.place(x=10, y=10)
 
- label1 = Label(ventana,text="Chapin Warrios, S.A", width=17,height=1)
- label1.config(fg="#FFEBD2",bg="#273248", font=("Bahnschrift SemiBold",22))
- label1.place(x = 380,y = 13)
+    img2 = Image.open("IMG\\robot.png")
+    new_img1 = img2.resize((85, 75))
+    render1 = ImageTk.PhotoImage(new_img1)
+    img3 = Label(ventana, image=render1)
+    img3.config(bg="#273248")
+    img3.image = render1
+    img3.place(x=610, y=0)
 
- img = Image.open("IMG\\CASILLAS.png")
- new_img = img.resize((225,150))
- render = ImageTk.PhotoImage(new_img)
- img1 = Label(ventana, image=render)
- img1.image = render
- img1.place(x=400, y = 485)
+    label1 = Label(ventana, text="Chapin Warrios, S.A", width=17, height=1)
+    label1.config(fg="#FFEBD2", bg="#273248",
+                  font=("Bahnschrift SemiBold", 24))
+    label1.place(x=320, y=13)
 
-#------------------------ TABLA CON CIUDADES --------------------------------
- label1 = Label(ventana,text="Lista de ciudades", width=15,height=1)
- label1.config(fg="#FFEBD2",bg="#273248", font=("Bahnschrift SemiBold",15))
- label1.place(x = 30,y = 60)
+    img = Image.open("IMG\\CASILLAS.png")
+    new_img = img.resize((150, 150))
+    render = ImageTk.PhotoImage(new_img)
+    img1 = Label(ventana, image=render)
+    img1.image = render
+    img1.place(x=420, y=485)
 
- table1 = ttk.Treeview(ventana, columns=("Dimenesion") ,height=19)
- table1.column("#0", width=115)
- table1.column("Dimenesion", width=115, anchor=CENTER)
- table1.heading("#0",text="Nombre", anchor=CENTER)
- table1.heading("Dimenesion",text="Dimenesion", anchor=CENTER)
- table1.place(x=30,y=110)
+# ------------------------ TABLA CON CIUDADES --------------------------------
+    label1 = Label(ventana, text="Lista de ciudades", width=15, height=1)
+    label1.config(fg="#FFEBD2", bg="#273248",
+                  font=("Bahnschrift SemiBold", 17))
+    label1.place(x=50, y=62)
 
- mostrar = Button(ventana, text="Mostrar Ciudad",width=15, height=2, command = get_data)
- mostrar.config(fg="Black",bg="#FC7643",font=("Bahnschrift SemiBold",10))
- mostrar.place(x= 80, y=530)
-#------------------------------------------------------------------------------
+    table1 = ttk.Treeview(ventana, columns=("Dimenesion"), height=19)
+    table1.column("#0", width=115)
+    table1.column("Dimenesion", width=115, anchor=CENTER)
+    table1.heading("#0", text="Nombre", anchor=CENTER)
+    table1.heading("Dimenesion", text="Dimenesion", anchor=CENTER)
+    table1.place(x=30, y=110)
 
- ventana.config( bg="#273248" )
- ventana.geometry('1050x650+200+40')
- ventana.mainloop()
+    label7 = Label(ventana, text="MAPA", width=15, height=1)
+    label7.config(fg="#FFEBD2", bg="#273248",
+                  font=("Bahnschrift SemiBold", 14))
+    label7.place(x=420, y=62)
+
+    mostrar = Button(ventana, text="Mostrar Ciudad",
+                     width=22, height=2, command=get_data)
+    mostrar.config(fg="Black", bg="#FC7643", font=("Bahnschrift SemiBold", 12))
+    mostrar.place(x=40, y=542)
+
+# ------------------------------OPCIONES PARA MISIONES -----------------------------
+    label2 = Label(ventana, text="Tipo de Mision", width=15, height=1)
+    label2.config(fg="#FFEBD2", bg="#273248",
+                  font=("Bahnschrift SemiBold", 11))
+    label2.place(x=730, y=62)
+    comboMision = ttk.Combobox(ventana, height=1, width=19, values=[
+                               '---', 'Rescate', 'Extraccion Recurso'], state="readonly", postcommand=Robots)
+    comboMision.current(0)
+    comboMision.place(x=740, y=90)
+
+    label3 = Label(ventana, text="Punto de entrada", width=15, height=1)
+    label3.config(fg="#FFEBD2", bg="#273248",
+                  font=("Bahnschrift SemiBold", 11))
+    label3.place(x=900, y=62)
+    comboEntradas = ttk.Combobox(ventana, height=1, width=19, values=[
+                                 '---'], state="readonly")
+    comboEntradas.current(0)
+    comboEntradas.place(x=900, y=90)
+
+    label4 = Label(ventana, text="Punto Civil", width=15, height=1)
+    label4.config(fg="#FFEBD2", bg="#273248",
+                  font=("Bahnschrift SemiBold", 11))
+    label4.place(x=730, y=120)
+    comboCiviles = ttk.Combobox(ventana, height=1, width=19, values=[
+                                '---'], state="readonly", postcommand=change_civiles)
+    comboCiviles.current(0)
+    comboCiviles.place(x=740, y=150)
+
+    label5 = Label(ventana, text="Punto de Recurso", width=15, height=1)
+    label5.config(fg="#FFEBD2", bg="#273248",
+                  font=("Bahnschrift SemiBold", 11))
+    label5.place(x=900, y=120)
+    comboRecursos = ttk.Combobox(ventana, height=1, width=19, values=[
+                                 '---'], state="readonly", postcommand=change_recurso)
+    comboRecursos.current(0)
+    comboRecursos.place(x=900, y=150)
+
+# ---------------------------------- OPCIONES DE LISTA DE ROBOTS  ---------------------------------
+
+    label6 = Label(ventana, text="Lista de Robots", width=15, height=1)
+    label6.config(fg="#FFEBD2", bg="#273248",
+                  font=("Bahnschrift SemiBold", 17))
+    label6.place(x=780, y=180)
+
+    table2 = ttk.Treeview(ventana, columns=("Tipo", "Capacidad"), height=14)
+    table2.column("#0", width=110)
+    table2.column("Tipo", width=105, anchor=CENTER)
+    table2.column("Capacidad", width=75, anchor=CENTER)
+    table2.heading("#0", text="NOMBRE", anchor=CENTER)
+    table2.heading("Tipo", text="TIPO", anchor=CENTER)
+    table2.heading("Capacidad", text="Capacidad", anchor=CENTER)
+    table2.place(x=740, y=220)
+
+    btnMision = Button(ventana, text="Mostrar Recorrido", width=22, height=2)
+    btnMision.config(fg="Black", bg="#FC7643",
+                     font=("Bahnschrift SemiBold", 12))
+    btnMision.place(x=790, y=560)
+
+    ventana.config(bg="#273248")
+    ventana.geometry('1050x650+200+40')
+    ventana.mainloop()
+
